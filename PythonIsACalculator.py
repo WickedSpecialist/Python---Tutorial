bd8581ef4646503e286a90605e9febf329339051
Wicked Specialist <WickedSpecialist@Shayans-MacBook-Pro.local>
2021-02-20 14:25:04 +0100

    commit PythonIsACalculator

---
+++ PythonIsACalculator.py
@@ -0,0 +1,50 @@
+
+
+def main():
+    print('Python is a Calculator: \n')
+
+    # Sum
+    print('Sum of numbers:')
+    print(12 + 45)
+    print(-12 + 45)
+    print(3.24545 + 234.233 + 873 + -23423)
+
+    # Sub
+    print('Sub of numbers:')
+    print(42 - 7)
+    print(-42 - 7)
+    print(-234.23 - 23.345 - 3.145)
+
+    # Mul
+    print('Mul of numbers:')
+    print(7 * 12)
+    print(12 * 12)
+    print(12 * 3454 * -345.234)
+
+    # Div
+    print('Div of numbers:')
+    print(91 / 14)
+    print(1024 / 8)
+    print(345.23 / -234 )
+
+    # Mod
+    print('Mod of numbers:')
+    print(235 % 7)
+    print(2048 % 16)
+    print(234 % 23 % 5)
+
+    # Exp
+    print('Exp of numbers:')
+    print(2 ** 10)
+    print(5 ** 5)
+    print(-4 ** 23)
+
+    # Floor Div
+    print('Floor Div of numbers:')
+    print(5 // 2)
+    print(256 // 8)
+    print(123.456 // 4.2)
+
+
+if __name__ == '__main__':
+    main()

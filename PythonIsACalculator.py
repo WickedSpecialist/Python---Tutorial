f99d5122838072b5d23936a8284fa5ece6a2fa87
Wicked Specialist <WickedSpecialist@Shayans-MacBook-Pro.local>
2021-02-20 20:15:06 +0100

    Python Is A Calculator

--- PythonIsACalculator.py
+++ PythonIsACalculator.py
@@ -1,31 +1,31 @@
 
 
 def main():
-    print('Python is a Calculator: \n')
+    print('Python is a Calculator:')
 
     # Addition
-    print('Sum of numbers:')
+    print('\nSum of numbers:')
     print(12 + 45)
     print(-12 + 45)
     print(3.24545 + 234.233 + 873 + -23423)
     print(985.726 + -2356.23 + 1.11)
 
     # Subtraction
-    print('Sub of numbers:')
+    print('\nSub of numbers:')
     print(42 - 7)
     print(-42 - 7)
     print(-234.23 - 23.345 - 3.145)
     print(34 - -23.1224 - 84.23)
 
     # Multiplication
-    print('Mul of numbers:')
+    print('\nMul of numbers:')
     print(7 * 12)
     print(12 * 12)
     print(12 * 3454 * -345.234)
     print(3.1415 * 3.1415 * -23.12 * -2.2)
 
     # Division
-    print('Div of numbers:')
+    print('\nDiv of numbers:')
     print(91 / 14)
     print(1024 / 8)
     print(345.23 / -235 )
@@ -37,21 +37,21 @@ def main():
     # print(65 / 0)
 
     # Modulo
-    print('Mod of numbers:')
+    print('\nMod of numbers:')
     print(235 % 7)
     print(2048 % 16)
     print(234 % 23 % 5)
     print(375.5 % 6 % 2)
 
     # Exponent
-    print('Exp of numbers:')
+    print('\nExp of numbers:')
     print(2 ** 10)
     print(5 ** 5)
     print(-4 ** 23)
     print((-2) ** 6)
 
     # Floor Division
-    print('Floor Div of numbers:')
+    print('\nFloor Div of numbers:')
     print(5 // 2)
     print(256 // 8)
     print(123.456 // 4.2)

31383f08918bc4c18ac099e2bc33cdd54ab1f063
Wicked Specialist <WickedSpecialist@Shayans-MacBook-Pro.local>
2021-02-20 20:11:16 +0100

    Python Is A Calculator

--- PythonIsACalculator.py
+++ PythonIsACalculator.py
@@ -3,47 +3,59 @@
 def main():
     print('Python is a Calculator: \n')
 
-    # Sum
+    # Addition
     print('Sum of numbers:')
     print(12 + 45)
     print(-12 + 45)
     print(3.24545 + 234.233 + 873 + -23423)
+    print(985.726 + -2356.23 + 1.11)
 
-    # Sub
+    # Subtraction
     print('Sub of numbers:')
     print(42 - 7)
     print(-42 - 7)
     print(-234.23 - 23.345 - 3.145)
+    print(34 - -23.1224 - 84.23)
 
-    # Mul
+    # Multiplication
     print('Mul of numbers:')
     print(7 * 12)
     print(12 * 12)
     print(12 * 3454 * -345.234)
+    print(3.1415 * 3.1415 * -23.12 * -2.2)
 
-    # Div
+    # Division
     print('Div of numbers:')
     print(91 / 14)
     print(1024 / 8)
     print(345.23 / -235 )
+    print(3.1415 * 2.718)
 
-    # Mod
+    # Obviously not possible but run the
+    # following code (Division by Zero)
+    # you have to uncomment it
+    # print(65 / 0)
+
+    # Modulo
     print('Mod of numbers:')
     print(235 % 7)
     print(2048 % 16)
     print(234 % 23 % 5)
+    print(375.5 % 6 % 2)
 
-    # Exp
+    # Exponent
     print('Exp of numbers:')
     print(2 ** 10)
     print(5 ** 5)
     print(-4 ** 23)
+    print((-2) ** 6)
 
-    # Floor Div
+    # Floor Division
     print('Floor Div of numbers:')
     print(5 // 2)
     print(256 // 8)
     print(123.456 // 4.2)
+    print(786 // 5)
 
 
 if __name__ == '__main__':

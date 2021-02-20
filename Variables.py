99cdc220ac19c986ccc40d1b1667565a428a9bf3
Wicked Specialist <WickedSpecialist@Shayans-MacBook-Pro.local>
2021-02-20 20:43:34 +0100

    Variables

--- Variables.py
+++ Variables.py
@@ -18,6 +18,22 @@ def main():
     print('c:', c)
     print('d:', d)
     print('e:', e)
+
+    print('\nCircle Example:')
+    pi = 3.1415
+    
+    radius_01 = 6
+    circumference_01 = 2 * pi * radius_01
+    surface_01 = pi * radius_01 * radius_01
+    
+    radius_02 = 13
+    circumference_02 = 2 * pi * radius_02
+    surface_02 = pi * radius_02 * radius_02
+    
+    print('1st Circle:')
+    print('\tCircumference:', circumference_01, '\tSurface:', surface_01)
+    print('2nd Circle:')
+    print('\tCircumference:', circumference_02, '\tSurface:', surface_02)
     
 if __name__ == '__main__':
     main()

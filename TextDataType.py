5f91bf81d3ea0bb10bb3cf10116cb6bf953aa6e5
Wicked Specialist <WickedSpecialist@Shayans-MacBook-Pro.local>
2021-02-20 22:36:40 +0100

    Text Data Type

---
+++ TextDataType.py
@@ -0,0 +1,28 @@
+
+
+def main():
+    print('str:')
+
+    print('\nThis is a string')
+    print('Next line is just a character:')
+    print('C')
+
+
+    print('String are very useful')
+    print(type('Strings are pretty cool'))
+    print(type('c'))
+
+
+    print('This ' + 'is ' + 'another ' + 'string')
+    print('Just Do It ' * 3)
+    
+    a = 'Drake'
+    b = ' is '
+    c = ' a good rapper.'
+
+    d = a + b + c
+    print('*' * 3 + d + '*' * 3)
+
+
+if __name__ == '__main__':
+    main()

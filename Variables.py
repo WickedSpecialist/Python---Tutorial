1dcd95c08946f4b4a3a16f99a6f41339b550cb2f
Wicked Specialist <WickedSpecialist@Shayans-MacBook-Pro.local>
2021-02-20 14:25:51 +0100

    Variables.py

---
+++ Variables.py
@@ -0,0 +1,23 @@
+
+
+def main():
+    print('Variables: \n')
+
+    x = 'Hello, World'
+    y = 123.234
+    z = -3.1415
+
+    a = x * 2
+    b = y + z
+    c = y ** z
+    d = y % z
+    e = y / z
+
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(e)
+    
+if __name__ == '__main__':
+    main()

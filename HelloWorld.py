9719bf9b8e9766bcfb8fdc2a2d9303a34b917cbf
Wicked Specialist <WickedSpecialist@Shayans-MacBook-Pro.local>
2021-02-20 20:13:02 +0100

    Hello World

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,7 +2,8 @@
 
 def main():
     print('\n\n\tHello, World!')
-    print('\tJustin is learning Python\n\n')
+    print('\tYOURNAME is learning Python')
+    print('\tYOURNAME is using Visual Studio Code\n\n')
 
 if __name__ == '__main__':
     main()
